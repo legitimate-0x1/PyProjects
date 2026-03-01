0f6dff4a2e7ddc8dfd3b7fa76cd509a028a05732
legitimate-0x1 <152633642+legitimate-0x1@users.noreply.github.com>
2026-03-01 11:26:49 +0300

    Create Potential Energy Calculator.py

---
+++ Potential Energy Calculator.py
@@ -0,0 +1,15 @@
+# Made by Sovf!
+
+print("Welcome to Potentia Energy Calculator!\n")
+
+while True:
+    Mass = float(input("Mass: "))
+    Acceleration = input("Acceleration: ").replace(" ", "")
+    Height = float(input("Height: "))
+
+    if Acceleration == "":
+        Acceleration = 10
+
+    Acceleration = float(Acceleration)
+
+    print("Potential Energy:", Mass * Acceleration * Height, "\n")
