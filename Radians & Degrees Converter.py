abf2784f0d02ec7430d04e05bb39466d0fa619b1
legitimate-0x1 <152633642+legitimate-0x1@users.noreply.github.com>
2026-03-01 12:31:40 +0300

    Update Radians & Degrees Converter.py

--- Radians & Degrees Converter.py
+++ Radians & Degrees Converter.py
@@ -2,12 +2,14 @@
 
 import math
 
-print("Welcome to Radians & Degrees Converter!\n\nThese are Convertions;\n\n1 - Degrees to Radians\n2 - Radians to Degrees\n")
+print("Welcome to Radians & Degrees Converter!\n\nThese are Convertions;\n\n 1 - Degrees to Radians\n 2 - Radians to Degrees\n\nRounding Enabled: Y/N\n")
 
 Tbl = [None, math.radians, math.degrees]
 
 while True:
     Convertion = int(input("Convertion: "))
+    RoundEnabled = input("Rounding Enabled: ").upper() == "Y"
     Number = float(input("Number: "))
+    Calculation = Tbl[Convertion](Number)
 
-    print("Calculation:", Tbl[Convertion](Number), "\n")
+    print("Calculation:", (RoundEnabled and round(Calculation) or Calculation), "\n")
