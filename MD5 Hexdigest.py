6e0f7f9044b3442042442f2867ba4f56dda1cc25
legitimate-0x1 <152633642+legitimate-0x1@users.noreply.github.com>
2026-03-02 16:04:52 +0300

    Update MD5 Hexdigest.py

--- MD5 Hexdigest.py
+++ MD5 Hexdigest.py
@@ -5,4 +5,4 @@ import hashlib
 print("Welcome to MD5 Hasher!\n")
 
 while True:
-    print("Factorial:", hashlib.md5(input("String: ").encode()).hexdigest(), "\n")
+    print("MD5 Hashed:", hashlib.md5(input("String: ").encode()).hexdigest(), "\n")
