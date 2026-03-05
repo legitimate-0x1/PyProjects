129e6fd7a2d28336715477fa378e2488ef23f69d
legitimate-0x1 <152633642+legitimate-0x1@users.noreply.github.com>
2026-03-05 22:16:17 +0300

    Create OpenAI Grammarly.py

---
+++ OpenAI Grammarly.py
@@ -0,0 +1,23 @@
+# Made by Sovf!
+
+from openai import OpenAI, OpenAIError
+
+API_Key = input("ChatGPT API Key: ")
+
+OpenAIClient = OpenAI(api_key = API_Key)
+
+print("Welcome to Grammarly!\n")
+
+while True:
+    Input = str(input("String: "))
+
+    try:
+        OpenAIAnswer = OpenAIClient.responses.create(
+            model = "gpt-5.2",
+            instructions = "You are a grammarly.",
+            input = Input,
+        )
+
+        print("Grammared Text:", OpenAIAnswer.output_text)
+    except OpenAIError as Reason:
+        print("Err:", Reason.code.upper())
