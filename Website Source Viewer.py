e36cc4abcb519d6cb4099bff022abcb2a1f559b5
legitimate-0x1 <152633642+legitimate-0x1@users.noreply.github.com>
2026-03-07 07:40:42 +0300

    Create Website Source Viewer.py

---
+++ Website Source Viewer.py
@@ -0,0 +1,24 @@
+# Made by Sovf!
+
+import urllib.request
+
+HTTPsURLTbl = ["http://", "https://", "www."]
+
+def AddHTTPs(URL):
+    URL = URL.lower()
+
+    for Index in HTTPsURLTbl:
+        URL = URL.replace(Index, "")
+
+    URL = "https://" + URL
+        
+    return URL
+
+def HttpGet(URL):
+    try:
+        return urllib.request.urlopen(AddHTTPs(URL)).read().decode("utf-8")
+    except:
+        return "An error occurred!"
+
+while True:
+    print("Website Source:", HttpGet(input("Website URL: ")), "\n")
